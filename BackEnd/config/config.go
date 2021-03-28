a19dd8fcaeed0884f9b6dd353726eaaf31a1efe5
Knight <knzphumthawan@gmail.com>
2021-03-28 17:21:08 +0700

    edit config database

--- BackEnd/config/config.go
+++ BackEnd/config/config.go
@@ -4,8 +4,8 @@ type Properties struct {
 	Port     string `env:"MY_APP_PORT" env-default:"8080"`
 	DBHost   string `env:"DB_HOST" env-default:"mongo"`
 	DBPort   string `env:"DB_PORT" env-default:"27017"`
-	DBName   string `env:"DB_NAME" env-default:"url_shorten"`
+	DBName   string `env:"DB_NAME" env-default:"urlShorten"`
 	DBUser   string `env:"DB_USER" env-default:"root"`
 	DBPass   string `env:"DB_PASS" env-default:"123456"`
-	DBColUrl string `env:"DB_Col_Url" env-default:"url_shorten"`
+	DBColUrl string `env:"DB_Col_Url" env-default:"URL"`
 }
