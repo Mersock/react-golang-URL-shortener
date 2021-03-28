5408c48cf2a08f16cad22d3dbf40b708ed21b52d
Knight <knzphumthawan@gmail.com>
2021-03-29 00:49:28 +0700

    find one face

--- BackEnd/dbiface/dbiface.go
+++ BackEnd/dbiface/dbiface.go
@@ -11,5 +11,9 @@ type (
 	CollectionAPI interface {
 		InsertOne(ctx context.Context, document interface{},
 			opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
+		Find(ctx context.Context, filter interface{},
+			opts ...*options.FindOptions) (*mongo.Cursor, error)
+		FindOne(ctx context.Context, filter interface{},
+			opts ...*options.FindOneOptions) *mongo.SingleResult
 	}
 )
