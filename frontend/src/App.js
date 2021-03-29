a95b1d5b698439c00fdb318bcf5d46d95ad5435c
Knight <knzphumthawan@gmail.com>
2021-03-29 15:07:11 +0700

    init admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from 'react-router-dom';
 import Landing from './components/Landing';
-import history from './history';
+import Admin from './components/Admin'
 import { Container } from 'reactstrap';
 
 
 function App() {
   return (
     <Container>
-      <Router history={history}>
+      <Router >
         <div>
           <Switch>
+            <Route path="/adminPage" exact component={Admin} />
             <Route path="/" exact component={Landing} />
           </Switch>
         </div>
