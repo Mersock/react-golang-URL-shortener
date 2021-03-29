b74582a2b895960e5f2573047b286387a21a5828
Knight <knzphumthawan@gmail.com>
2021-03-29 14:59:11 +0700

    init boostratp container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import history from './history';
+import { Container } from 'reactstrap';
+
 
 function App() {
   return (
-    <div>
+    <Container>
       <Router history={history}>
         <div>
           <Switch>
@@ -13,7 +15,7 @@ function App() {
           </Switch>
         </div>
       </Router>
-    </div>
+    </Container>
   );
 }
 
