d0caf067ab81e786d2048ed346f76efc36a87ead
Knight <knzphumthawan@gmail.com>
2021-03-29 15:41:43 +0700

    add handle edit url input

--- frontend/src/components/Landing/FormInput.js
+++ frontend/src/components/Landing/FormInput.js
@@ -3,9 +3,31 @@ import { Button, Form, FormGroup, Label, Input, Alert, FormFeedback } from 'reac
 
 const FormInput = (props) => {
     const [inputValid, setinputValid] = useState(false)
+    const [urlInput, seturlInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const url = isUrl(urlInput)
+        if (!url) {
+            setinputValid(true)
+        } else {
+            setinputValid(false)
+        }
+
+    }
+
+    const isUrl = (val) => {
+        const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+        const regex = new RegExp(expression);
+        if (val.match(regex)) {
+            return true
+        }
+        return false
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label for="originalURL">
                         <h2>Original Url</h2>
@@ -13,7 +35,7 @@ const FormInput = (props) => {
                             <span>Ex: <a href="https://www.youtube.com">https://www.youtube.com</a></span>
                         </div>
                     </Label>
-                    <Input invalid={inputValid} type="text" name="originalURL" id="originalURL" placeholder="originalURL" />
+                    <Input onChange={(e) => seturlInput(e.target.value)} invalid={inputValid} type="text" name="originalURL" id="originalURL" placeholder="originalURL" />
                     <FormFeedback>Oh noes! that Url is invalid</FormFeedback>
                 </FormGroup>
                 <FormGroup>
