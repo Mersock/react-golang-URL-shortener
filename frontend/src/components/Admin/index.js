4ae211ad94230221a7e0c53d318aeb85ee4e7a75
Knight <knzphumthawan@gmail.com>
2021-03-29 21:00:24 +0700

    add filter admin page

--- frontend/src/components/Admin/index.js
+++ frontend/src/components/Admin/index.js
@@ -1,17 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Col, Row } from 'reactstrap'
 import TableList from './tableList'
-import { getUrl } from '../../actions'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const Index = (props) => {
-    const dispatch = useDispatch()
     const tableList = useSelector(state => state.urlShortens.listUrl)
-
-    useEffect(() => {
-        dispatch(getUrl())
-        //eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     return (
         <Row>
