b56dbc5e08841a95f01cf5f8caf087f7911c4a28
Knight <knzphumthawan@gmail.com>
2021-03-29 20:10:30 +0700

    fix bug landing

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
-import FormInput from './FormInput'
+import Header from './header'
+import FormInput from './formInput'
 
 const index = (props) => {
     return (
